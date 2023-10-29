530447dc24e5f50b894b31618a3711e5269e1e15
stewie <kirill12093@mail.ru>
2023-10-29 18:31:55 +0300

    lab2: realize kLargest

--- cmd/lab2/k_largest.go
+++ cmd/lab2/k_largest.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	intHeap "GoBasics/pkg/heap"
+	"container/heap"
+)
+
 func findKthLargest(nums []int, k int) int {
-	//your code should be there
-	return 0
+	minHeap := make(intHeap.MinHeap, k)
+	for i := 0; i < k; i++ {
+		minHeap[i] = nums[i]
+	}
+
+	heap.Init(&minHeap)
+	for i := k; i < len(nums); i++ {
+		if minHeap[0] < nums[i] {
+			minHeap[0] = nums[i]
+			heap.Fix(&minHeap, 0)
+		}
+	}
+	return minHeap[0]
 }
